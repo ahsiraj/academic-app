8cc8a3af79db8e0f00da7dce2e430164cab86743
ask@ceconline.edu <ask@ceconline.edu>
2021-10-19 12:00:27 +0530

    users role with enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  #ref https://stackoverflow.com/questions/24479839/multiple-types-of-users-on-ruby-on-rails/24480937
+  enum role: [:student, :faculty, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :student
+  end
 end
