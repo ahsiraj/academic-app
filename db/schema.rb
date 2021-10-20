f28e520d6d3fb19f1b24f3090204e1a631d20f05
ask@ceconline.edu <ask@ceconline.edu>
2021-10-20 13:42:50 +0530

    rails g active:admin User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_073111) do
+ActiveRecord::Schema.define(version: 2021_10_19_161738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,7 +79,6 @@ ActiveRecord::Schema.define(version: 2021_10_19_073111) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
     t.integer "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
